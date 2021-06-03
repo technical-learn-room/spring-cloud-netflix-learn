0560f934fcc2da13b142bf441b3d93ba515d0816
jaychy-yy <0816jinjin@gmail.com>
2021-06-03 17:10:33 +0900

    reactor: 메인 클래스 이름을 SpringCloudLearnApplication에서 EurekaServerApplication으로 변경.

--- eureka-server/src/main/java/com/me/eureka/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/me/eureka/EurekaServerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class SpringCloudLearnApplication {
+public class EurekaServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpringCloudLearnApplication.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
